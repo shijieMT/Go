6a0085d1110db93337d8e663a0cb83c45c99cb50
史杰 <85970157+shijieMT@users.noreply.github.com>
2024-04-25 17:10:10 +0800

    Add files via upload

---
+++ 基础语法/8.线程安全.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var num_func8 int
+var wait_func8 sync.WaitGroup
+var lock_func8 sync.Mutex
+
+func add_func8() {
+	lock_func8.Lock()
+	for i := 0; i < 1000000; i++ {
+		num_func8++
+	}
+	lock_func8.Unlock()
+
+	wait_func8.Done()
+}
+func reduce_func8() {
+	lock_func8.Lock()
+	for i := 0; i < 1000000; i++ {
+		num_func8--
+	}
+	lock_func8.Unlock()
+	wait_func8.Done()
+}
+func 线程安全() {
+	var t = time.Now()
+	wait_func8.Add(2)
+	go add_func8()
+	go reduce_func8()
+	wait_func8.Wait()
+	fmt.Println(time.Since(t))
+	fmt.Println(num_func8)
+}
+
+/*
+我们不能在并发模式下读写map
+如果要这样做
+给读写操作加锁
+使用sync.Map
+加锁
+*/
+var mp = map[string]string{}
+
+/*
+如果我们在一个协程函数下，读写map就会引发一个错误
+concurrent map read and map write
+*/
+func reader() {
+	for i := 0; i < 3000000; i++ {
+		fmt.Println(mp["time"])
+	}
+	wait_func8.Done()
+}
+func writer() {
+	for i := 0; i < 3000000; i++ {
+		mp["time"] = time.Now().Format("15:04:05")
+	}
+	wait_func8.Done()
+}
+
+var mp_sync = sync.Map{}
+
+func reader_sync() {
+	for {
+		fmt.Println(mp_sync.Load("time"))
+	}
+	wait.Done()
+}
+func writer_sync() {
+	for {
+		mp_sync.Store("time", time.Now().Format("15:04:05"))
+	}
+	wait.Done()
+}
+
+func 线程安全下的map() {
+	//wait_func8.Add(2)
+	//go writer()
+	//go reader()
+	//wait_func8.Wait()
+
+	wait_func8.Add(2)
+	go reader_sync()
+	go writer_sync()
+	wait_func8.Wait()
+}
+func func8() {
+	//线程安全()
+	线程安全下的map()
+}
